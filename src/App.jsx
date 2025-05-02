a21aba6343ce6e2ce681342cd578e2140581b132
sena <gulozseven@gmail.com>
2025-05-02 22:35:35 +0300

    setup & packages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import workintech from '/workintech.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   const [count, setCount] = useState(0)
