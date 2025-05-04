5ec8a6f8ad6491cb1b77b84895097b0d6d2470cb
sena <gulozseven@gmail.com>
2025-05-04 11:56:09 +0300

    submitted form IT1.5 completed
    .pizza-details should be revisited

--- src/siparis-olustur/submitted.jsx
+++ src/siparis-olustur/submitted.jsx
@@ -4,25 +4,26 @@ import './submittedForm.css';
 const Submitted = () => {
 
     const location = useLocation();
-    const { size, dough, toppings } = location.state || {}; 
+    const { size, dough, toppings } = location.state || {};
 
     return (
         <div className="submitted-container">
-            <img className="teknolojik-logo" src="images/iteration-1-images/logo.svg" alt="Teknolojik Yemekler"/>
+            <img className="teknolojik-logo" src="images/iteration-1-images/logo.svg" alt="Teknolojik Yemekler" />
 
             <section className="confirmation-messages">
                 <p className="on-the-way">lezzetin yolda</p>
                 <h3 className="received-message">SİPARİŞ ALINDI</h3>
-                <hr />    
-            </section> 
-            
-            <p className="pizza-name">Position Absolute Acı Pizza</p>
-            <section className="boyut-hamur-malz">
-                <p>Boyut: {size}</p>
-                <p>Hamur: {dough}</p>
-                <p>Malzemeler: {toppings?.join(', ')}</p>
+                <hr />
             </section>
-        </div>
+            <section className="pizza-infos">
+                <p className="pizza-name">Position Absolute Acı Pizza</p>
+                <section className="pizza-details">
+                    <p>Boyut: <span style={{ fontWeight: 'bold' }}>{size}</span></p>
+                    <p>Hamur: <span style={{ fontWeight: 'bold' }}>{dough}</span></p>
+                    <p>Malzemeler: <span style={{ fontWeight: 'bold' }}>{toppings?.join(', ')}</span></p>
+                </section>
+            </section>
+        </div >
     )
 }
 
